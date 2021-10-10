b55e57455e60d7ea5511a28b11f6ea58398ac764
Rahu20057 <92220976+Rahu20057@users.noreply.github.com>
2021-10-10 09:11:40 +0530

    Create script.py

--- script.py
+++ script.py
@@ -52,7 +52,7 @@ Send <i>/help</i> for commands and more details...</b>
 """
 
 
-    ABOUT_MSG = """📌<b>Name :</b> <a href='https://t.me/Filter_cs_bot'>Autofilter #V3</a> 
+    ABOUT_MSG = """📌<b>Name :</b> <a href='https://t.me/Filter_cs_bot'>Filter Beta #V3</a> 
 
 📌<b>Creater :</b> <a href='https://t.me/rahul_p_r_00'>This Person</a> 
 
